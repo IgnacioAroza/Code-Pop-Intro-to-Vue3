b9cac1e7e5133419f9273e56ea9bacdd23c446eb
Ignacio Aroza <ignacioaroza.ia@gmail.com>
2023-05-09 10:36:53 -0300

    L3 terminado

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const app = Vue.createApp({
     data(){
         return{
             product: 'Socks',
-            description: 'Unos calidos soquetes'
+            description: 'Unos calidos soquetes',
+            // solution
+            url: 'https://www.vuemastery.com/'
+            // solution
         }
     }
 })
